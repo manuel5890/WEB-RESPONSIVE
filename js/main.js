9f74bae4da30e9c41e3a5156a6e18ccc881311ad
Manuel Calderon <108032857+manuel5890@users.noreply.github.com>
2025-04-11 14:45:02 -0500

    COMMIT

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function modalVideo(){
+    let selector = document.getElementById("modal-1")
+
+    selector.classList.toggle("active")
+}
+
+function menuMobile(){
+    let selector = document.getElementById("cont-menu-mobile")
+
+    selector.classList.toggle("active")
+}
